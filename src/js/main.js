1074a47249b29258927b71d31bec0336244dcc94
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2022-07-21 23:23:50 +0200

    Add navigation color change effect

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ const btnNavbar = document.querySelector('#menu-btn');
 const searchForm = document.querySelector('.search-form');
 const bntSearch = document.querySelector('#search-btn');
 
+const header = document.querySelector('.header');
+
 btnNavbar.addEventListener('click', () => {
   navbar.classList.toggle('active');
   searchForm.classList.remove('active');
@@ -18,3 +20,7 @@ window.onscroll = () => {
   navbar.classList.remove('active');
   searchForm.classList.remove('active');
 }
+
+window.addEventListener('scroll', function () {
+  header.classList.toggle('sticky', window.scrollY > 0);
+})
